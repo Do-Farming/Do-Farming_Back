5eec4c5865e67819c0b4a51c44a7d0473f7a5392
chaeyeon-yang <myday0827@naver.com>
2024-06-28 14:27:33 +0900

    feat: 챌린지 그룹 생성 스웨거 테스트

--- src/main/java/com/hana/api/group/controller/GroupController.java
+++ src/main/java/com/hana/api/group/controller/GroupController.java
@@ -3,7 +3,10 @@ package com.hana.api.group.controller;
 
 import com.hana.api.auth.Auth;
 import com.hana.api.group.dto.GroupRequestDto;
+import com.hana.api.group.service.GroupService;
+import com.hana.common.config.BaseException;
 import com.hana.common.config.BaseResponse;
+import com.hana.common.config.BaseResponseStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,10 +23,13 @@ import java.util.UUID;
 @Slf4j
 public class GroupController {
 
+    private final GroupService groupService;
+
     @Operation(summary = "챌린지 그룹 생성")
     @PostMapping("/create")
     public BaseResponse.SuccessResult<String> createGroup(@RequestBody GroupRequestDto.GroupCreateReq request, @Auth String userCode) {
         UUID uuid = UUID.fromString(userCode);
+        groupService.createGroup(request, uuid);
         return BaseResponse.success("챌린지 그룹 생성 성공");
     }
 }
