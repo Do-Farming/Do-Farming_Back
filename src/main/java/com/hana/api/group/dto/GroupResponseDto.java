93e5df6a93b26a62abdd778d886e09e29e323bc1
chaeyeon-yang <myday0827@naver.com>
2024-06-28 17:30:43 +0900

    feat: 각 그룹에 참여인원을 받아와 그룹 목록 조회 시 같이 조회하기

--- src/main/java/com/hana/api/group/dto/GroupResponseDto.java
+++ src/main/java/com/hana/api/group/dto/GroupResponseDto.java
@@ -1,7 +1,34 @@
 package com.hana.api.group.dto;
 
+import com.hana.api.group.entity.Group;
+import lombok.*;
 
+import java.time.LocalDateTime;
 
 public class GroupResponseDto {
 
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Builder
+    public static class GetGroupListRes {
+        private long id;
+        private String groupName;
+        private Integer groupNumber;
+        private Integer participantNumber;
+        private String title;
+        private LocalDateTime createdDate;
+
+        public static GetGroupListRes from(Group group, int participantNumber) {
+            return GetGroupListRes.builder()
+                    .id(group.getId())
+                    .groupName(group.getGroupName())
+                    .groupNumber(group.getGroupNumber())
+                    .participantNumber(participantNumber)
+                    .title(group.getTitle())
+                    .createdDate(group.getCreatedDate())
+                    .build();
+        }
+
+    }
 }
