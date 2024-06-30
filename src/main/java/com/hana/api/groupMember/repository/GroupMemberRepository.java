7c5a7293dfcfb36f1aec037ddf1ebadd12b515d6
chaeyeon-yang <myday0827@naver.com>
2024-07-01 01:31:23 +0900

    feat: 이미 가입된 회원 에러 케이스 처리

--- src/main/java/com/hana/api/groupMember/repository/GroupMemberRepository.java
+++ src/main/java/com/hana/api/groupMember/repository/GroupMemberRepository.java
@@ -3,8 +3,15 @@ package com.hana.api.groupMember.repository;
 import com.hana.api.groupMember.entity.GroupMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.UUID;
 
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
     @Query("SELECT COUNT(gm) FROM group_member gm WHERE gm.group.id = :groupId")
     int countByGroupId(Long groupId);
+
+    @Query("SELECT COUNT(gm) > 0 FROM group_member gm WHERE gm.group.id = :groupId AND gm.user.userCode = :userCode")
+    boolean isAlreadyJoin(@Param("groupId") Long groupId, @Param("userCode") UUID userCode);
+
 }
