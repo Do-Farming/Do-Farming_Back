16541ad6a936b4cc596effa9fba5a9c80d12a561
JeongHeum Byun <quswjdgma83@gmail.com>
2024-07-01 15:32:42 +0900

    이전대화 목록 반환하도록 설정 및 캐싱하는 기능 추가

--- src/main/java/com/hana/api/gpt/controller/GptController.java
+++ src/main/java/com/hana/api/gpt/controller/GptController.java
@@ -1,12 +1,16 @@
 package com.hana.api.gpt.controller;
 
+import com.hana.api.auth.Auth;
 import com.hana.api.gpt.service.OpenAiService;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/gpt")
@@ -16,7 +20,7 @@ public class GptController {
     private OpenAiService openAiService;
 
     @GetMapping("/chat")
-    public Mono<String> chat(@RequestParam String prompt) {
-        return openAiService.getChatResponse(prompt);
+    public Mono<Map<String, Object>> chat(@RequestParam String prompt,@Parameter(hidden = true) @Auth String userCode) {
+        return openAiService.getChatResponse(prompt, userCode);
     }
 }
--- src/main/java/com/hana/api/group/repository/GroupRepository.java
+++ src/main/java/com/hana/api/group/repository/GroupRepository.java
@@ -7,5 +7,4 @@ import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
     List<Group> findByIsPublicTrue();
-
 }
