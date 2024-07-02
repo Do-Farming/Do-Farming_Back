3f4a8439879eef8efb2cad1b3183f41a3aca7436
JeongHeum Byun <quswjdgma83@gmail.com>
2024-07-02 17:04:04 +0900

    chore: 개발 중 JWT토큰 우회 위한 화이트 리스트 등록

--- src/main/java/com/hana/api/auth/filter/CustomerAuthenticationFilter.java
+++ src/main/java/com/hana/api/auth/filter/CustomerAuthenticationFilter.java
@@ -24,7 +24,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class CustomerAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider tokenProvider;
     private final List<String> EXCLUDE_URL = List.of("/**", "/api/v1/auth", "/api/v1/customer/signup", "/favicon",
-            "/api/v1/sms", "/swagger", "/v3", "/api/v1/developer", "/api/ocr");
+            "/api/v1/sms", "/swagger", "/v3", "/api/v1/developer", "/api/ocr","/api/gpt/generate-image","/api/gpt/chat");
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
