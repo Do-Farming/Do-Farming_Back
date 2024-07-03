8c95b623513e2970495c0236af8d841600c91e30
JeongHeum Byun <quswjdgma83@gmail.com>
2024-07-03 17:59:01 +0900

    feat: 챌린지 기록 관련 변경사항

---
+++ src/main/java/com/hana/api/challenge/repository/ChallengeRecordRepository.java
@@ -0,0 +1,15 @@
+package com.hana.api.challenge.repository;
+
+import com.hana.api.challenge.entity.ChallengeRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ChallengeRecordRepository extends JpaRepository<ChallengeRecord, Long> {
+    List<ChallengeRecord> findByChallengeTypeAndChallengeDate(int challengeType, LocalDate challengeDate);
+}
+
