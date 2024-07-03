e73e397ace82f9523d7b7e96b1709422f8c70307
JeongHeum Byun <quswjdgma83@gmail.com>
2024-07-03 17:53:55 +0900

    chore: 쓸모없는 코드2

--- src/main/java/com/hana/api/user/repository/UserRepository.java
+++ src/main/java/com/hana/api/user/repository/UserRepository.java
@@ -5,6 +5,7 @@ import com.hana.api.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
