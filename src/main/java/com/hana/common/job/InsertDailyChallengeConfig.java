9a233f50f2d3ee9d1e8988651c8dd23b0d56eb7a
JeongHeum Byun <quswjdgma83@gmail.com>
2024-07-04 11:18:17 +0900

    chore: 테이블 미추가로 인해 오류발생하던 코드 주석 해제

--- src/main/java/com/hana/common/job/InsertDailyChallengeConfig.java
+++ src/main/java/com/hana/common/job/InsertDailyChallengeConfig.java
@@ -1,36 +1,36 @@
-//package com.hana.common.job;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.batch.core.Job;
-//import org.springframework.batch.core.Step;
-//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-//import org.springframework.batch.core.job.builder.JobBuilder;
-//import org.springframework.batch.core.repository.JobRepository;
-//import org.springframework.batch.core.step.builder.StepBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.transaction.PlatformTransactionManager;
-//
-//@Configuration
-//@EnableBatchProcessing
-//@RequiredArgsConstructor
-//public class InsertDailyChallengeConfig {
-//
-//    private final JobRepository jobRepository;
-//    private final PlatformTransactionManager transactionManager;
-//    private final InsertDailyChallengeTasklet insertDailyChallengeTasklet;
-//
-//    @Bean
-//    public Job insertDailyChallengeJob() {
-//        return new JobBuilder("insertDailyChallengeJob", jobRepository)
-//                .start(insertDailyChallengeStep())
-//                .build();
-//    }
-//
-//    @Bean
-//    public Step insertDailyChallengeStep() {
-//        return new StepBuilder("insertDailyChallengeStep", jobRepository)
-//                .tasklet(insertDailyChallengeTasklet, transactionManager)
-//                .build();
-//    }
-//}
+package com.hana.common.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableBatchProcessing
+@RequiredArgsConstructor
+public class InsertDailyChallengeConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final InsertDailyChallengeTasklet insertDailyChallengeTasklet;
+
+    @Bean
+    public Job insertDailyChallengeJob() {
+        return new JobBuilder("insertDailyChallengeJob", jobRepository)
+                .start(insertDailyChallengeStep())
+                .build();
+    }
+
+    @Bean
+    public Step insertDailyChallengeStep() {
+        return new StepBuilder("insertDailyChallengeStep", jobRepository)
+                .tasklet(insertDailyChallengeTasklet, transactionManager)
+                .build();
+    }
+}
--- src/main/java/com/hana/common/job/InsertDailyChallengeTasklet.java
+++ src/main/java/com/hana/common/job/InsertDailyChallengeTasklet.java
@@ -1,23 +1,23 @@
-//package com.hana.common.job;
-//
-//import com.hana.api.challenge.service.ChallengeRecordService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.batch.core.StepContribution;
-//import org.springframework.batch.core.repository.JobRepository;
-//import org.springframework.batch.core.scope.context.ChunkContext;
-//import org.springframework.batch.core.step.tasklet.Tasklet;
-//import org.springframework.batch.repeat.RepeatStatus;
-//import org.springframework.stereotype.Component;
-//import org.springframework.transaction.PlatformTransactionManager;
-//
-//@RequiredArgsConstructor
-//@Component
-//public class InsertDailyChallengeTasklet implements Tasklet {
-//
-//    private final ChallengeRecordService challengeRecordService;
-//    @Override
-//    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-//        challengeRecordService.insertDailyChallengeRecord();
-//        return RepeatStatus.FINISHED;
-//    }
-//}
+package com.hana.common.job;
+
+import com.hana.api.challenge.service.ChallengeRecordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@RequiredArgsConstructor
+@Component
+public class InsertDailyChallengeTasklet implements Tasklet {
+
+    private final ChallengeRecordService challengeRecordService;
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        challengeRecordService.insertDailyChallengeRecord();
+        return RepeatStatus.FINISHED;
+    }
+}
