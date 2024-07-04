3a381a5dc06ca189fbe026b1d958a343dbc24b40
chaeyeon-yang <myday0827@naver.com>
2024-07-04 16:11:10 +0900

    feat: 기상미션 기상 인증 Redis 저장

--- src/main/java/com/hana/api/challenge/wakeup/controller/WakeupController.java
+++ src/main/java/com/hana/api/challenge/wakeup/controller/WakeupController.java
@@ -1,14 +1,17 @@
 package com.hana.api.challenge.wakeup.controller;
 
 
+import com.hana.api.auth.Auth;
+import com.hana.api.challenge.wakeup.dto.WakeupResponseDto;
 import com.hana.api.challenge.wakeup.service.WakeupChallengeService;
 import com.hana.common.config.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,4 +26,27 @@ public class WakeupController {
     public BaseResponse.SuccessResult<String> todayObject() {
         return BaseResponse.success(wakeupChallengeService.getTodayObject());
     }
+
+    @Operation(summary = "기상 인증")
+    @PostMapping("/certificate")
+    public BaseResponse.SuccessResult<String> wakeupCertificate(@Parameter(hidden = true) @Auth String userCode) {
+        UUID uuid = UUID.fromString(userCode);
+        wakeupChallengeService.postWakeupCertificate(uuid);
+        return BaseResponse.success("기상 인증 성공");
+    }
+
+//    @Operation(summary = "사용자 기상 시간 조회")
+//    @GetMapping("/getMyWakeupTime")
+//    public BaseResponse.SuccessResult<WakeupResponseDto.WakeupCertificateDto> getWakeupCertificate(@Auth String userCode) {
+//        UUID uuid = UUID.fromString(userCode);
+//        return BaseResponse.success(wakeupChallengeService.getWakeupCertificate(uuid));
+//    }
+//
+//    @Operation(summary = "사용자 기상 시간 삭제")
+//    @DeleteMapping("/deleteMyWakeupTime")
+//    public BaseResponse.SuccessResult<String> deleteWakeupCertificate(@Auth String userCode) {
+//        UUID uuid = UUID.fromString(userCode);
+//        wakeupChallengeService.deleteWakeupCertificate(uuid);
+//        return BaseResponse.success("사용자 기상 시간 삭제 성공");
+//    }
 }
--- src/main/java/com/hana/api/challenge/wakeup/service/WakeupChallengeService.java
+++ src/main/java/com/hana/api/challenge/wakeup/service/WakeupChallengeService.java
@@ -1,12 +1,22 @@
 package com.hana.api.challenge.wakeup.service;
 
+import com.hana.api.challenge.wakeup.dto.WakeupResponseDto;
 import com.hana.api.challenge.wakeup.repository.WakeupChallengeRepository;
+import com.hana.api.group.entity.Group;
+import com.hana.api.groupMember.repository.GroupMemberRepository;
+import com.hana.api.user.entity.User;
+import com.hana.api.user.repository.UserRepository;
+import com.hana.common.config.BaseException;
+import com.hana.common.config.BaseResponseStatus;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
@@ -14,7 +24,11 @@ import java.util.*;
 @Slf4j
 public class WakeupChallengeService {
 
+    private final UserRepository userRepository;
+    private final GroupMemberRepository groupMemberRepository;
     private final WakeupChallengeRepository wakeupChallengeRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+
 
     public static String getTodayObject() {
         List<String> detectList = Arrays.asList("toothbrush", "book", "clock", "spoon", "hair drier");
@@ -23,8 +37,43 @@ public class WakeupChallengeService {
         Random rand = new Random();
         String selectedObject = detectList.get(rand.nextInt(detectList.size()));
 
-
         return selectedObject;
     }
+
+    public void postWakeupCertificate(UUID userCode) {
+        User user = userRepository.findByUserCode(userCode)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.USERS_EMPTY_USER_ID));
+        String userId = user.getPhoneNumber();
+        Long groupId = groupMemberRepository.findGroupByUserId(userId);
+
+        WakeupResponseDto.WakeupCertificateDto certificate = new WakeupResponseDto.WakeupCertificateDto();
+        certificate.setWakeupTime(LocalDateTime.now());
+        certificate.setUserId(userId);
+        String key = "wakeup:certificate:group:" + groupId + ":userId:" + userId;
+        // Redis에 26시간 동안 유지
+        redisTemplate.opsForValue().set(key, certificate, 26, TimeUnit.HOURS);
+    }
+
+//    public WakeupResponseDto.WakeupCertificateDto getWakeupCertificate(UUID userCode) {
+//        User user = userRepository.findByUserCode(userCode)
+//                .orElseThrow(() -> new BaseException(BaseResponseStatus.USERS_EMPTY_USER_ID));
+//        String userId = user.getPhoneNumber();
+//        Long groupId = groupMemberRepository.findGroupByUserId(userId);
+//
+//        String key = "wakeup:certificate:group:" + groupId + ":userId:" + userId;
+//        return (WakeupResponseDto.WakeupCertificateDto) redisTemplate.opsForValue().get(key);
+//    }
+//
+//    public void deleteWakeupCertificate(UUID userCode) {
+//        User user = userRepository.findByUserCode(userCode)
+//                .orElseThrow(() -> new BaseException(BaseResponseStatus.USERS_EMPTY_USER_ID));
+//
+//        String userId = user.getPhoneNumber();
+//        Long groupId = groupMemberRepository.findGroupByUserId(userId);
+//
+//        String key = "wakeup:certificate:group:" + groupId + ":userId:" + userId;
+//        redisTemplate.delete(key);
+//    }
+
 
 }
