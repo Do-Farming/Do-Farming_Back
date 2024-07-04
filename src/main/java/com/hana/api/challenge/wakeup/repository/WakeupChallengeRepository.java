5dac2c616597120af7d08dbb9ce1b8231c209699
JeongHeum Byun <quswjdgma83@gmail.com>
2024-07-04 11:17:14 +0900

    chore: 메인 폴더 구조 변경에 따른 수정

--- src/main/java/com/hana/api/challenge/wakeup/repository/WakeupChallengeRepository.java
+++ src/main/java/com/hana/api/challenge/wakeup/repository/WakeupChallengeRepository.java
@@ -2,6 +2,12 @@ package com.hana.api.challenge.wakeup.repository;
 
 import com.hana.api.challenge.wakeup.entity.WakeupChallenge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
 public interface WakeupChallengeRepository extends JpaRepository<WakeupChallenge, Long> {
+    List<WakeupChallenge> findByWakeupDateAndGroupId(LocalDate wakeupDate, Long groupId);
 }
