663d76f6197634f15bbc053b83bd557c142c2ea2
chaeyeon-yang <myday0827@naver.com>
2024-07-04 16:09:46 +0900

    feat: objectMapper를 통해 LocalDateTime 직렬화

---
+++ src/main/java/com/hana/common/config/JacksonConfig.java
@@ -0,0 +1,18 @@
+package com.hana.common.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return Jackson2ObjectMapperBuilder.json()
+                .modules(new JavaTimeModule())
+                .build();
+    }
+}
