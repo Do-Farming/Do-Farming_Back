c455533917c97c3dc019f03b3761ac5f2cea8b43
Minjoo-kang123 <rkdalswn0833@gmail.com>
2024-07-05 16:20:10 +0900

    fix:ãSendNotification 수정

--- src/main/java/com/hana/api/firebase/service/PushNotificationService.java
+++ src/main/java/com/hana/api/firebase/service/PushNotificationService.java
@@ -11,11 +11,10 @@ public class PushNotificationService {
     public void sendNotificationToDevice(String token, String title, String message) {
         sendPushNotification(token, title, message);
     }
-    //해당 service 이용해서 영어만!!! 넣어서 보내면 제대로 push 감 token은 user에서 deviedId 테이블 사용
-    //title은 큰 글씨 message는 작은 글씨
+
     private void sendPushNotification(String token, String title, String message) {
         RestTemplate restTemplate = new RestTemplate();
-        String payload = String.format("{\"to\":\"%s\",\"sound\":\"default\",\"title\":\"%s\",\"body\":\"%s\"}", token, title, message);
+        String payload = String.format("{\"to\":\"%s\",\"sound\":\"default\",\"title\":\"%s\",\"body\":\"%s\"}", "ExponentPushToken[-FSFxvP0NsUarmmpNqcOlm]", "Dofarming", "message");
         restTemplate.postForEntity(EXPO_PUSH_URL, payload, String.class);
     }
 }
