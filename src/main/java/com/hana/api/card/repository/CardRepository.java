0057ee428ec2ad2ecf15445cd5e8886cfacbb5be
LimEunSang <dmstkd2905@naver.com>
2024-07-07 02:00:08 +0900

    feat: Tag를 포함하는 혜택을 가진 신용카드 중 1위를 반환하는 쿼리

--- src/main/java/com/hana/api/card/repository/CardRepository.java
+++ src/main/java/com/hana/api/card/repository/CardRepository.java
@@ -1,6 +1,5 @@
 package com.hana.api.card.repository;
 
-import com.hana.api.account.entity.Account;
 import com.hana.api.card.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,13 +7,11 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
     List<Card> findCardByType(String type);
 
-//    @Query("SELECT c FROM card c where c.type = :type LIMIT :count")
-//    List<Card> findCardByTypeAndCount(@Param(value = "type") String type, @Param(value = "count") int count);
+    @Query("SELECT c FROM card c WHERE c.benefit LIKE :tag AND c.type = 'CRD' ORDER BY c.ranking ASC LIMIT 1")
+    List<Card> findTopRankingCardByTag(@Param(value = "tag") String tag);
 }
