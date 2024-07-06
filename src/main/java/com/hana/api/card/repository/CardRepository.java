5b8764492d3d0afb7bb1a546d633ba5fe7395cda
LimEunSang <dmstkd2905@naver.com>
2024-07-07 02:01:22 +0900

    del: repository @Param annotation

--- src/main/java/com/hana/api/card/repository/CardRepository.java
+++ src/main/java/com/hana/api/card/repository/CardRepository.java
@@ -3,7 +3,6 @@ package com.hana.api.card.repository;
 import com.hana.api.card.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +12,5 @@ public interface CardRepository extends JpaRepository<Card, Long> {
     List<Card> findCardByType(String type);
 
     @Query("SELECT c FROM card c WHERE c.benefit LIKE :tag AND c.type = 'CRD' ORDER BY c.ranking ASC LIMIT 1")
-    List<Card> findTopRankingCardByTag(@Param(value = "tag") String tag);
+    List<Card> findTopRankingCardByTag(String tag);
 }
