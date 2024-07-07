269ade9df4dee3227f88b14c8345389d4a3f19ae
JeongHeum Byun <quswjdgma83@gmail.com>
2024-07-07 13:29:45 +0900

    feat: 만보계 웹소켓 연결을 통한 실시간 업데이트

---
+++ src/main/java/com/hana/common/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.hana.common.config;
+
+import com.hana.api.challenge.websocket.PedometerWebSocketHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(pedometerWebSocketHandler(), "/pedometer").setAllowedOrigins("*");
+    }
+
+    @Bean
+    public PedometerWebSocketHandler pedometerWebSocketHandler() {
+        return new PedometerWebSocketHandler();
+    }
+}
+
